6e6c7aa48442060a97414c2f6ad0b1f522b78734
Matthew Morgan <matthewmorganjoh@gmail.com>
2023-03-15 08:58:10 -0700

    complete functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(defaultVariable = "roller-skate"){
+    return `This Saturday, I want to ${defaultVariable}!`; 
+}
+
+function mondayWork(defaultActivity = "go to the office"){
+    return `This Monday, I will ${defaultActivity}.`;
+}
+
+function wrapAdjective(flair = "*"){
+    return function (adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
